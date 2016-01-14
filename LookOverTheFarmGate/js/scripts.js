8586b99f098b03714b50c1ebb7d38d7d2886703f
bobbyquennell <bobbyquennell@gmail.com>
2016-01-14 15:33:08 +1100

    final touch with radius button and overlay

--- LookOverTheFarmGate/js/scripts.js
+++ LookOverTheFarmGate/js/scripts.js
@@ -1,12 +1,13 @@
 var OpenPopupWindow = function(){
     $(".popup-content").show();
-    //$("div.popup-container").show();
+    $(".popup-overlay").show();
 };
 var closePopupWindwod = function(){
     $(".popup-content").hide();
-    //$("div.popup-container").show();
+    $(".popup-overlay").hide();
 };
 $(document).ready(function(){
     $("input#btnPopup").on('click', OpenPopupWindow);
     $("a.popup-close").on('click', closePopupWindwod);
 });
+
